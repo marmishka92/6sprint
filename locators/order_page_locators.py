3164fa7eb92d05fa86a0e56d5db2a78b0c6e8278
marmishka92 <katrinclub92@gmail.com>
2025-08-15 15:37:45 +0300

    Update order_page_locators.py

--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -2,24 +2,27 @@ from selenium.webdriver.common.by import By
 
 
 class OrderPageLocators:
-    # Форма "Для кого самокат":
-    NAME_LOCATOR = By.XPATH, '//input[@placeholder="* Имя"]'  # поле Имя
-    LAST_NAME_LOCATOR = By.XPATH, '//input[@placeholder="* Фамилия"]'  # поле Фамилия
-    ADDRESS_LOCATOR = By.XPATH, '//input[@placeholder="* Адрес: куда привезти заказ"]'  # поле Адресс
-    METRO_LOCATOR = By.XPATH, '//input[@placeholder="* Станция метро"]'  # поле Метро
-    METRO_LIST_ELM_LOCATOR = By.XPATH, '//*[contains(@class, "Order_Text") and text()="{}"]'  # элемент списка метро
-    MOBILE_LOCATOR = By.XPATH, '//input[@placeholder="* Телефон: на него позвонит курьер"]'  # поле Телефон
-    NEXT_BUTTON_LOCATOR = (By.XPATH,
-                           '//*[contains(@class, "Order_NextButton")]/*[contains(@class, "Button_Button")]')  # кнопка Далее
-    # Форма "Про аренду":
-    DATE_RENT_LOCATOR = By.XPATH, '//input[@placeholder="* Когда привезти самокат"]'  # поле Когда привезти
-    DATE_FROM_CALENDAR_LOCATOR = (By.XPATH,
-                                  '//*[contains(@class, "datepicker__day") and text()="{}"]')  # дата в календаре поля Когда привезти
-    RENTAL_PERIOD_LOCATOR = By.XPATH, '//*[@class="Dropdown-placeholder"and text()="* Срок аренды"]'  # Срок аренды
-    RENTAL_PERIOD_LIST_ELM_LOCATOR = By.XPATH, '//*[@class ="Dropdown-option" and text()="{}"]'  # элемент списка срока аренды
-    COLOUR_LOCATOR = By.ID, '{}'   # чекбоксы цвета
-    COMMENT_LOCATOR = By.XPATH, '//input[@placeholder="Комментарий для курьера"]'  # поле Комментарий
-    ORDER_BUTTON_LOCATOR = By.XPATH, '//*[contains(@class, "Button_Middle") and text()="Заказать"]'  # кнопка Заказать
-    # Модальные окна
-    YES_BUTTON = By.XPATH, '//*[contains(@class,"Order_Modal")]//*[text()="Да"]'  # кнопка Да
-    CONFIRMATION_SUCCESS_TEXT_LOCATOR = By.XPATH, '//*[contains(@class, "Order_ModalHeader")]'  # локатор текста Заказ оформлен
+    # Экран "Для кого самокат"
+    title_page_personal_info = (By.XPATH, "//div[text()='Для кого самокат' and contains(@class, 'Order_Header')]")
+    field_name = (By.XPATH, "//input[@placeholder='* Имя']")
+    field_lastname = (By.XPATH, "//input[@placeholder='* Фамилия']")
+    field_address = (By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']")
+    field_metro = (By.XPATH, "//input[@placeholder='* Станция метро']")
+    select_item_in_dropdown_metro = (By.XPATH, ".//li[@class='select-search__row']")
+    field_phone = (By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']")
+    button_next = (By.XPATH, "//button[text()='Далее']")
+
+    # Экран "Про аренду"
+    title_page_rent_info = (By.XPATH, "//div[text()='Про аренду' and contains(@class, 'Order_Header')]")
+    field_date = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']")
+    calendar = (By.XPATH, "//div[@class='react-datepicker-popper']")
+    calendar_date = (By.XPATH, "//div[contains(@class, 'react-datepicker') and contains(@tabindex, '0')]")
+    field_rental_period = (By.XPATH, ".//div[text()='* Срок аренды']")
+    dropdown_item_rental_period = (By.XPATH, ".//div[@class = 'Dropdown-menu']/div[text() ='трое суток']")
+    checkbox_grey_color_scooter = (By.XPATH, "//input[@id='grey']")
+    field_comment = (By.XPATH, "//input[@placeholder='Комментарий для курьера']")
+    button_make_order = (By.XPATH, "//div[contains(@class, 'Order_Buttons')]/button[text()='Заказать']")
+
+    # Подтверждение заказа
+    button_confirm_order = (By.XPATH, "//button[text()='Да']")
+    button_check_status_of_order = (By.XPATH, ".//*[text()='Посмотреть статус']")
